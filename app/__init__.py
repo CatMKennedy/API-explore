034341482b28116456b59920a2ccff80b6e2415f
Catriona Kennedy <catm.kennedy@gmail.com>
2024-04-26 10:44:53 +0100

    updated some comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
+'''
+Uses application factory pattern. Follows approximately the guidelines in 
+Miguel Grinberg's mega-tutorial at https://blog.miguelgrinberg.com/, and also the
+DigitalOcean tutorial on structuring a large Flask application.
+
+'''
 from flask import Flask
 
 from config import Config
@@ -8,6 +14,7 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
+# Called by api.py in the top level directory
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
